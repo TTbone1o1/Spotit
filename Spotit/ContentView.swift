57e76b8fcc05cfb34f55ba158afc93f2292fdc0d
Abraham May <AbrahamMay1015@gmail.com>
2026-07-09 13:03:46 -0500

    Initial Commit

---
+++ Spotit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotit
+//
+//  Created by Abraham May on 7/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotit/SpotitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotitApp.swift
+//  Spotit
+//
+//  Created by Abraham May on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
